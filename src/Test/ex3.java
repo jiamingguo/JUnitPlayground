3df09cf032382dcd725d800c0a9006fa73c185e3
jiamingguo <gjm1993217@gmail.com>
2017-03-16 02:02:12 -0400

    3/15/2017

--- src/Test/ex3.java
+++ src/Test/ex3.java
@@ -19,6 +19,9 @@ public class ex3 {
         // the method could be overloaded to compare different types. int, String...
         // also can be used to compare two objects, as long as a ".equals" method is defined (overrided) in object
 
+        // 0 is the "delta". Since we are comparing double, it's not very possible to exactly same.
         assertEquals(area,12,0);
+        // also: assertEquals("Failed not equal", area, 12, 0); with a failure message
+        // Moreover: assertFalse(area>15), assertTrue(area+2<15);
     }
 }
